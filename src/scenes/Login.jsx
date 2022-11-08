c53922f004617501dd22c004fbce5248205e6111
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-08 11:22:41 -0500

    login with fb auth, routes to 'protected.jsx'

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-export default function Login() {
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnWrL5zG0eweq2E_52fHwVhHwgeBcrXfU",
+  authDomain: "fb-auth-tj.firebaseapp.com",
+  projectId: "fb-auth-tj",
+  storageBucket: "fb-auth-tj.appspot.com",
+  messagingSenderId: "842395049547",
+  appId: "1:842395049547:web:58309b5a8515a6895c37ce"
+};
+
+export default function Login({ setUser }) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const handleLogin = (e) => {
+  const handleLogin = async(e) => {
     e.preventDefault()
+    const app = initializeApp(firebaseConfig) //connects to Firebase
+    const auth = getAuth(app) //connects to Firebase Auth
+    const response = await signInWithEmailAndPassword(auth, email, password)
+      .catch(alert);
+    setUser(response.user)
   }
   return (
     <>
